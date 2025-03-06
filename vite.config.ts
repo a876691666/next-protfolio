6e95b9395bc7f19f87acd66b9805af1f1918e920
Ajia0 <928200728@qq.com>
2025-03-06 17:15:03 +0800

    更新vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || ''
+const base = repoName ? `/${repoName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.BASE_URL || base,
   resolve: {
     alias: {
       'next-portfolio': resolve(__dirname, './lib/index.ts')
